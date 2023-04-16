b759a17c344a6ff05e167721f07278ea964c818c
bodig <bodiga.stas@mail.ru>
2023-04-16 13:45:06 +0300

    сделал дз

---
+++ src/Book.java
@@ -0,0 +1,32 @@
+public class Book {
+    private String bookName;
+    private Author authorName;
+    private int year;
+
+    public Book(String bookName, Author authorName, int year) {
+        this.bookName = bookName;
+        this.authorName = authorName;
+        this.year = year;
+
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public Author getAuthor() {
+        return authorName ;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public String toString() {
+        return getBookName()+" "+authorName+" "+getYear();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        Author authorWarAndPeace = new Author("Л.Н.Толстой");
+        Book warAndPeace = new Book("Война и мир", authorWarAndPeace, 1867);
+        Author authorOffenceAndPunishment = new Author("Ф.М.Достоевский");
+        Book OffenceAndPunishment = new Book("Преступление и наказание",authorOffenceAndPunishment,1866);
+        System.out.println(warAndPeace.getAuthor() + " " + warAndPeace.getBookName() + " " + warAndPeace.getYear());
+        System.out.println(OffenceAndPunishment.getAuthor() + " " + OffenceAndPunishment.getBookName() + " " + OffenceAndPunishment.getYear());
+        OffenceAndPunishment.setYear(2002);
+        System.out.println(OffenceAndPunishment.getAuthor() + " " + OffenceAndPunishment.getBookName() + " " + OffenceAndPunishment.getYear());
+
     }
-}
+    }
